a8c5c652ee8135b3fab26dd1a82efb18f5d13f5c
Norbert Bota <botanorbert1@gmail.com>
2018-04-17 22:55:28 +0300

    Update binary_gap.py

--- binary_gap.py
+++ binary_gap.py
@@ -1,15 +1,12 @@
-def solution(N):
-    big_gap = 0
-    gap = 0
+def solution(N, big_gap = 0, gap = 0):
     
-    for idx, i in enumerate('{0:1b}'.format(N)):
+    for i in '{0:1b}'.format(N):
         if i == '0':
             gap += 1
-    
-        elif i != 0 and idx > 0:
-            if big_gap <= gap:
-                big_gap = gap
-                gap = 0
+        elif big_gap <= gap:
+            big_gap = gap
+            gap = 0
+        else:
             gap = 0
 
     return(big_gap)
