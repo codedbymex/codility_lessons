fcacd164e0958d05f515f0bf87523807096d9b51
Norbert Bota <botanorbert1@gmail.com>
2018-04-18 12:24:50 +0300

    Update cyclic_rotation.py

--- cyclic_rotation.py
+++ cyclic_rotation.py
@@ -1,5 +1,4 @@
 def solution(A, K):
     if A:
-        next_list = lambda x: x[-1:] + x[:-1]
-        A = [next_list(A) for i in range(K)][-1]
+        A = [A[-1:] + A[:-1] for i in range(K)][-1]
     return A
